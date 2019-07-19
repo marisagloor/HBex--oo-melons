cdf4d8f3560bedb420dead944af85c401b032021
Your Name <you@example.com>
2019-07-19 13:00:14 -0700

    adjusted get_total

--- melons.py
+++ melons.py
@@ -1,5 +1,6 @@
 """Classes for melon orders."""
 
+
 class AbstractMelonOrder():
     """Any melon order"""
 
@@ -13,8 +14,11 @@ class AbstractMelonOrder():
         """Calculate price, including tax."""
 
         base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
 
+        if self.species == 'Christmas melon':
+            base_price *= 1.5
+
+        total = (1 + self.tax) * self.qty * base_price
         return total
 
     def mark_shipped(self):
@@ -44,3 +48,19 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
+
+    def get_total(self):
+
+        total = super().get_total()
+
+        if self.qty < 10:
+            total += 3
+
+
+# class GovernmentMelonOrder(AbstractMelonOrder):
+#     """class for melons with specail government tax"""
+#     passed_inspection = None
+#     tax = 0
+
+#     def mark_inspection(self, passed):
+#         self.passed_inspection = passed
